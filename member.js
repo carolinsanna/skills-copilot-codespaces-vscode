a8ad287e8f153ca0b505a7a9cb25b17e8d9e771b
Carolina Dias <59510922+carolinsanna@users.noreply.github.com>
2023-12-06 23:51:16 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function memberSkills(){
+    return{
+        restrict:'E',
+        templateUrl: 'modules/skills/views/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: 'true',
+        scope: {
+            member: '='
+        }
+    };
+}
